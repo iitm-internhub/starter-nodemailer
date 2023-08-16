664c08f0449f8ab37cf88ea19b7102bd71059f80
Kam Lasater <kam.lasater@cyclic.sh>
2023-08-16 10:00:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# starter-nodemailer
+
+This is just a demonstration of using nodemailer on Cyclic. For production use you will want to protect the sending route.
+
+## Setup
+
+1. Create application specific password: https://security.google.com/settings/security/apppasswords
+1. Set env variables
+1. Test the `/send` route
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const nodemailer = require('nodemailer');
+const smtp = require('nodemailer-smtp-transport');
+
+const app = express()
+
+
+
+
+app.use('/send', async (req, res) => {
+  const username = process.env.SMTP_USERNAME || null
+  const password = process.env.SMTP_PASSWORD || null
+  if (!username || !password) {
+    throw new Error('SMTP_USERNAME and SMTP_PASSWORD must be set')
+  }
+
+
+  const transporter = nodemailer.createTransport(smtp({
+    service: 'gmail',
+    auth: {
+        user: username,
+        pass: password
+    }
+  }));
+
+  const text = `Just saying hello. \n\n- Your friend, ${username}`;
+
+  var mailOptions = {
+    from: username,
+    to: username,
+    // bcc: '<bcc email addres>',
+    subject: 'starter-nodemailer',
+    text
+  };
+
+  const sendRes = await transporter.sendMail(mailOptions);
+
+  // const errorResponse = {
+  //     statusCode: 500,
+  //     body: JSON.stringify({
+  //       error: error.message,
+  //     }),
+  //   };
+  // }
+  // const successResponse = {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: `Email processed succesfully!`
+  //   }),
+  // };
+
+  res.json(sendRes).end()
+})
+
+app.use('*', (req, res) => {
+  res.json({ msg: 'no route handler found' }).end()
+})
+
+// Start the server
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`index.js listening on ${port}`)
+})
